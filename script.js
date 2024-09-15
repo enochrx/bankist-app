204c9ad93b5abc5792a8c2ea652cb73293731686
Damilare Enoch <drreayy@gmail.com>
2024-09-15 18:22:21 +0100

    remainder

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ const displayMovements = function (movements, sort = false) {
           <div class="movements__type movements__type--${depWith}">${
       i + 1
     } ${depWith}</div>
-          <div class="movements__value">${mov}€</div>
+          <div class="movements__value">${mov.toFixed(2)}€</div>
         </div>`;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -111,7 +111,7 @@ const displayMovements = function (movements, sort = false) {
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 //Display Summary
@@ -124,7 +124,7 @@ const calcDisplaySummary = function (acc) {
   const out = acc.movements
     .filter(move => move < 0)
     .reduce((accu, move) => accu + move, 0);
-  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(move => move > 0)
@@ -212,7 +212,7 @@ btnTransfer.addEventListener("click", function (e) {
 
 btnLoan.addEventListener("click", function (e) {
   e.preventDefault();
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     currentAccount.movements.push(amount);
     //
